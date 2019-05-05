0509d8f9215a2450ed354dbdb3135d153000e7bd
Alberto Faria <albertofaria.dev@gmail.com>
2019-05-06 00:45:07 +0100

    Informative commit message.

--- tp-2/fileshare/src/fileshare/core/JobState.java
+++ tp-2/fileshare/src/fileshare/core/JobState.java
@@ -48,7 +48,7 @@ public class JobState
     private Phase phase;
 
     private long totalBytes;
-    private final AtomicLong transferredBytes;
+    private AtomicLong transferredBytes;
 
     private long startNanos;
 
@@ -310,179 +310,25 @@ public class JobState
     @Override
     public synchronized JobState clone()
     {
-        final var other = super.clone();
+        final var other = new JobState(job);
+
+        other.phase = this.phase;
+
+        other.totalBytes = this.totalBytes;
+        other.transferredBytes = new AtomicLong(this.transferredBytes.get());
+
+        other.startNanos = this.startNanos;
+
+        other.markNanos = this.markNanos;
+        other.markTransferredBytes = this.markTransferredBytes;
+
+        other.endNanos = this.endNanos;
+        other.endTransferredBytes = this.endTransferredBytes;
+
+        other.errorMessage = this.errorMessage;
+
+        return other;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-    /**
-     * TODO: document
-     *
-     * @param job TODO: document
-     * @param totalBytes TODO: document
-     * @param transferredBytes TODO: document
-     * @param throughput TODO: document
-     * @param errorMessage TODO: document
-     *
-     * @throws NullPointerException TODO: document
-     * @throws IllegalArgumentException TODO: document
-     */
-    public JobState(
-        Job job,
-        Optional< Long > totalBytes,
-        long transferredBytes,
-        Optional< Long > throughput,
-        Optional< String > errorMessage
-        )
-    {
-        // validate arguments
-
-        Objects.requireNonNull(totalBytes);
-        Objects.requireNonNull(throughput);
-        Objects.requireNonNull(errorMessage);
-
-        if (totalBytes.isEmpty())
-        {
-            if (transferredBytes != 0)
-                throw new IllegalArgumentException("TODO: write");
-
-            if (throughput.isPresent())
-                throw new IllegalArgumentException("TODO: write");
-        }
-        else
-        {
-            if (totalBytes.get() < 0)
-                throw new IllegalArgumentException("TODO: write");
-
-            if (transferredBytes < 0)
-                throw new IllegalArgumentException("TODO: write");
-
-            if (transferredBytes > totalBytes.get())
-                throw new IllegalArgumentException("TODO: write");
-
-            if (throughput.isPresent() && throughput.get() < 0)
-                throw new IllegalArgumentException("TODO: write");
-        }
-
-        // initialize instance
-
-        this.job              = job;
-
-        this.totalBytes       = totalBytes;
-        this.transferredBytes = transferredBytes;
-        this.throughput       = throughput;
-
-        this.errorMessage     = errorMessage;
-    }
-
-    /**
-     * TODO: document
-     *
-     * Is non-negative.
-     *
-     * @return TODO: document
-     */
-    public Optional< Long > getTotalBytes()
-    {
-        return this.totalBytes;
-    }
-
-    /**
-     * TODO: document
-     *
-     * @param totalBytes TODO: document
-     */
-    public void setTotalBytes(Optional< Long > totalBytes)
-    {
-        this.totalBytes = totalBytes;
-    }
-
-    /**
-     * TODO: document
-     *
-     * If total bytes is empty, this is 0.
-     *
-     * Otherwise is non-negative and lower than or equal to total bytes.
-     *
-     * @return TODO: document
-     */
-    public long getTransferredBytes()
-    {
-        return this.transferredBytes;
-    }
-
-    /**
-     * TODO: document
-     *
-     * @param transferredBytes TODO: document
-     */
-    public void setTransferredBytes(long transferredBytes)
-    {
-        this.transferredBytes = transferredBytes;
-    }
-
-    /**
-     * TODO: document
-     *
-     * Not a perfect representation of the throughput, and the interval to which
-     * the throughput is relative is left unspecified.
-     *
-     * In bytes per second.
-     *
-     * @return TODO: document
-     */
-    public Optional< Long > getThroughput()
-    {
-        return this.throughput;
-    }
-
-    /**
-     * TODO: document
-     *
-     * @param throughput TODO: document
-     */
-    public void setThroughput(Optional< Long > throughput)
-    {
-        this.throughput = throughput;
-    }
-
-    /**
-     * TODO: document
-     *
-     * @return TODO: document
-     */
-    public Optional< String > getErrorMessage()
-    {
-        return this.errorMessage;
-    }
-
-    /**
-     * TODO: document
-     *
-     * @param errorMessage TODO: document
-     */
-    public void setErrorMessage(Optional< String > errorMessage)
-    {
-        this.errorMessage = errorMessage;
-    }
-
-    /**
-     * TODO: document
-     *
-     * Always between 0 and 100, inclusive. Also 0 if totalBytes is empty. Only
-     * 100 if the transfer is complete.
-     *
-     * @return TODO: document
-     */
 }
 
 /* -------------------------------------------------------------------------- */
