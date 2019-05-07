ecd03f110ca009d28333070d234c5fee62a29033
Alberto Faria <albertofaria.dev@gmail.com>
2019-05-07 12:13:22 +0100

    Informative commit message.

--- tp-2/fileshare/src/fileshare/transport/ReliableSocket.java
+++ tp-2/fileshare/src/fileshare/transport/ReliableSocket.java
@@ -11,8 +11,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.OptionalInt;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
