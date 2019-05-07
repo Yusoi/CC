2ab6884c625788024804dbcbe69a9ca61b66baee
Alberto Faria <albertofaria.dev@gmail.com>
2019-05-07 11:34:42 +0100

    Informative commit message.

--- tp-2/fileshare/src/fileshare/transport/ReliableSocket.java
+++ tp-2/fileshare/src/fileshare/transport/ReliableSocket.java
@@ -503,6 +503,83 @@ public class ReliableSocket implements AutoCloseable
     {
 
     }
+
+    private void sendConn(
+        byte[] packetBuffer,
+        Endpoint remoteEndpoint,
+        int clientConnectionSeqnum
+    )
+    {
+        // TODO: implement
+    }
+
+    private void sendConnReject(
+        byte[] packetBuffer,
+        Endpoint remoteEndpoint,
+        int serverConnectionSeqnum
+    )
+    {
+        // TODO: implement
+    }
+
+    private void sendConnAccept(
+        byte[] packetBuffer,
+        Endpoint remoteEndpoint,
+        int clientConnectionSeqnum,
+        int serverConnectionSeqnum
+    )
+    {
+        // TODO: implement
+    }
+
+    private void sendConnAcceptAck(
+        byte[] packetBuffer,
+        Endpoint remoteEndpoint,
+        int serverConnectionSeqnum
+    )
+    {
+        // TODO: implement
+    }
+
+    private void sendData(
+        byte[] packetBuffer,
+        Endpoint remoteEndpoint,
+        int remoteConnectionSeqnum,
+        int payloadSeqnum,
+        byte[] payloadBuffer,
+        int payloadLength
+    )
+    {
+        // TODO: implement
+    }
+
+    private void sendDataAck(
+        byte[] packetBuffer,
+        Endpoint remoteEndpoint,
+        int remoteConnectionSeqnum,
+        int payloadSeqnum
+    )
+    {
+        // TODO: implement
+    }
+
+    private void sendDisc(
+        byte[] packetBuffer,
+        Endpoint remoteEndpoint,
+        int remoteConnectionSeqnum
+    )
+    {
+        // TODO: implement
+    }
+
+    private void sendDiscAck(
+        byte[] packetBuffer,
+        Endpoint remoteEndpoint,
+        int remoteConnectionSeqnum
+    )
+    {
+        // TODO: implement
+    }
 }
 
 /* -------------------------------------------------------------------------- */
