7c1635539453f42cfe4474728b87ae8f7bdd7309
Alberto Faria <albertofaria.dev@gmail.com>
2019-05-11 15:38:06 +0100

    Informative commit message.

--- tp-2/entrega/CC-TP2-PL59-Codigo/fileshare.sh
+++ tp-2/entrega/CC-TP2-PL59-Codigo/fileshare.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-rlwrap java -jar fileshare.jar "${@}"
+set -e
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+rlwrap java -jar "${script_dir}/fileshare.jar" "${@}"
